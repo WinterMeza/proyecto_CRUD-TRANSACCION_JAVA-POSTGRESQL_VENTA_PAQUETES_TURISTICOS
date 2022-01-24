8243d6004614bad78fa47df51ccc27c716257347
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-01-24 00:01:22 -0500

    CREACION DE BASE DE DATOS VENTA_PAQUETES_TURISTICO

---
+++ CREACION DE BASE DE DATOS.sql
@@ -0,0 +1,12 @@
+-- Database: ventas_paquetes_turisticos_et
+
+-- DROP DATABASE ventas_paquetes_turisticos_et;
+
+CREATE DATABASE ventas_paquetes_turisticos_et
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
